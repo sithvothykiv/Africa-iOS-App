eafb68ef0d8ad74cf40d1a833c0b56de8e9a0e98
Sithvothy <kiv@msn.com>
2022-08-05 11:45:37 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Sithvothy Infinity on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
